0f52783dc41b4bb6fa8b096797ab98ce5b7d4ab3
Steven <steven.k.rhodes6@gmail.com>
2018-08-09 00:09:52 -0400

    Added InputManager script based on online reference. Script enables 2D drag and drop functionality for both mouse and touch input.
    Basic mouse input is tested and working, touch input is untested. A simple test object is now in the test scene, and it can be clicked and dragged around.
    The input manager will need to be refined greatly as we develop other scripts before it can provide all its intended functionality.

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+public class InputManager : MonoBehaviour
+{
+    private bool draggingItem = false;
+    private GameObject draggedObject;
+    private Vector2 touchOffset;
+
+    void Update()
+    {
+        if (HasInput)
+        {
+            DragOrPickUp();
+        }
+        else
+        {
+            if (draggingItem)
+                DropItem();
+        }
+    } //end Update
+
+    Vector2 CurrentTouchPosition
+    {
+        get
+        {
+            Vector2 inputPos;
+            inputPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            return inputPos;
+        }
+    } //end CurrentTouchPosition
+
+    private void DragOrPickUp()
+    {
+        var inputPosition = CurrentTouchPosition;
+
+        if (draggingItem)
+        {
+            draggedObject.transform.position = inputPosition + touchOffset;
+        }
+        else
+        {
+            RaycastHit2D[] touches = Physics2D.RaycastAll(inputPosition, inputPosition, 0.5f);
+            if (touches.Length > 0)
+            {
+                var hit = touches[0];
+                if (hit.transform != null)
+                {
+                    draggingItem = true;
+                    draggedObject = hit.transform.gameObject;
+                    touchOffset = (Vector2)hit.transform.position - inputPosition;
+                    draggedObject.transform.localScale = new Vector3(1.2f, 1.2f, 1.2f);
+                }
+            }
+        }
+    } //end DragOrPickup
+
+    private bool HasInput
+    {
+        get
+        {
+            // returns true if either the mouse button is down or at least one touch is felt on the screen
+            return Input.GetMouseButton(0);
+        }
+    } //end HasInput
+
+    void DropItem()
+    {
+        draggingItem = false;
+        draggedObject.transform.localScale = new Vector3(1f, 1f, 1f);
+    } //end DropItem
+} //end InputManager class
