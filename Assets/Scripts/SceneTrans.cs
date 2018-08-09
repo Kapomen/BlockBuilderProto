f4372c81885540c154214ac5b0cbffdba90d662d
Sicheng Ma <masicheng1995@gmail.com>
2018-08-09 15:18:49 -0400

    MENU UI

---
+++ Assets/Scripts/SceneTrans.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SceneTrans : MonoBehaviour
+{
+
+    public void changeToScene(string sceneToChangeTo)
+    {
+        SceneManager.LoadScene(sceneToChangeTo);
+    }
+}
