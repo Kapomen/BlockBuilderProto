b970b3c7682fb93dee187dc5665f3258d564fcd9
Sicheng Ma <masicheng1995@gmail.com>
2018-08-20 14:22:36 -0400

    making score working

--- Assets/Scripts/ScoreCounting.cs
+++ Assets/Scripts/ScoreCounting.cs
@@ -1,43 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ScoreCounting : MonoBehaviour {
+    public Text scoreTextMesh;
+    private int Score;
 
-    public TextMesh scoreTextMesh;
-    private string scoreText;
-    private int Score = 0;
     // Use this for initialization
     void Start()
     {
-        scoreText = scoreTextMesh.text;
-        updateScore(0);
-
+        Score = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        GameObject checkblock = GameObject.Find("Game Manager");
+        GameManager currentblocks = checkblock.GetComponent<GameManager>();
+        updateScore(currentblocks.BlocksInPlay);
     }
-    /*
-    public void ModScore(int _value)
-    {
-        Score += _value;
-        playerScore.updateScore(Score);
-    }
-
-    void OnTriggerEnter(Collider other)
-    {
-        if (other.tag == "Brick")
-        {
-            ModScore(100);
-        }
-    }*/
 
     public void updateScore(int value)
     {
-        scoreTextMesh.text = scoreText;
-        scoreTextMesh.text += value;
+        
     }
 }
